5eb97f855453637ee4a578b8bbb3bff51b88ba80
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-24 00:16:46 -0500

    1

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Editor from "./Editor";
+import Editor from "./editor";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 function App() {

8de1d129dd45f69a97c852eb79a096228e3385cb
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-23 21:53:10 -0500

    adding code editor

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,44 +1,51 @@
 import * as React from "react";
-import { Helmet } from "react-helmet";
-import { StaticImage } from "gatsby-plugin-image";
+import {Helmet} from "react-helmet";
+import {StaticImage} from "gatsby-plugin-image";
 import Layout from "../components/layout";
-import DomTraversal from "../components/domTraversal";
 import ProjectsLayout from "../components/projectsLayout";
+import DomTraversal from "../components/domTraversal";
+import CodeEditor from "../components/codeEditor/app";
+
 
 const Code = () => {
   return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Nifty Code</title>
-        <link rel="canonical" href="http://ayse.site/code" />
-      </Helmet>
-      <Layout pageTitle="Nifty Code">
-        <div className="pb-6 md:py-6">
-          {/*  DOM Traversal */}
-          <div className="text-center text-3xl pb-10">DOM Traversal</div>
-          <div className=" flex justify-center align-middle">
-            <StaticImage
-              className="max-w-sm md:max-w-xl"
-              src="../images/nodeTree.png"
-              alt="Node Tree"
-              objectPosition="center center"
-            />
+      <>
+        <Helmet>
+          <meta charSet="utf-8"/>
+          <title>Nifty Code</title>
+          <link rel="canonical" href="http://ayse.site/code"/>
+        </Helmet>
+        <Layout pageTitle="Nifty Code">
+          <div className="pb-6 md:py-6">
+            {/*  DOM Traversal */}
+            <div className="text-center text-3xl pb-10">DOM Traversal</div>
+            <div className=" flex justify-center align-middle">
+              <StaticImage
+                  className="max-w-sm md:max-w-xl"
+                  src="../images/nodeTree.png"
+                  alt="Node Tree"
+                  objectPosition="center center"
+              />
+            </div>
+            <DomTraversal/>
           </div>
-          <DomTraversal />
-        </div>
-        <div className="pb-10 max-w-2xl lg:max-w-4xl m-auto text-center">
-          I created this page to familiarize myself with traversing the dom. All
-          of the logic used in this page stems from the DOM. I used a for loop
-          to traverse the node tree. For the fun facts, I used
-          getElementsByTagName to find the " &lt;td&gt; element and an if
-          statement to change the text color.
-        </div>
-        <hr />
+          <div className="pb-10 max-w-2xl lg:max-w-4xl m-auto text-center">
+            I created this page to familiarize myself with traversing the dom. All
+            of the logic used in this page stems from the DOM. I used a for loop
+            to traverse the node tree. For the fun facts, I used
+            getElementsByTagName to find the " &lt;td&gt; element and an if
+            statement to change the text color.
+          </div>
+          <hr/>
 
-        <ProjectsLayout />
-      </Layout>
-    </>
+          {/*  CSS Trick */}
+          <div className="text-center text-3xl py-10">
+            Responsive Layout Without Media Queries
+          </div>
+          <CodeEditor/>
+          <ProjectsLayout/>
+        </Layout>
+      </>
   );
 };
 

6020b159fd2d49d3d056e4c537144cf1d81ba926
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-28 00:43:00 -0500

    1.0

--- src/components/codeEditor/codeEditor.js
+++ src/components/codeEditor/codeEditor.js
@@ -1,77 +1,19 @@
 import React, { useEffect, useState } from "react";
 import SplitPane from "react-split-pane";
-import { useDebounce } from "../../utils/useDebounce";
 import * as styles from "./codeEditor.module.css";
 
-import { CssEditor, HtmlEditor, JavascriptEditor } from "./editors";
-
 const CodeEditor = () => {
-  const [heightValue, setHeightValue] = useState("485px");
-
-  const [htmlValue, setHtmlValue] = useState("");
-  const [jsValue, setJsValue] = useState("");
-  const [cssValue, setCssValue] = useState("");
-  const [outputValue, setOutputValue] = useState("");
-
-  const debouncedHtml = useDebounce(htmlValue, 1000);
-  const debouncedJs = useDebounce(jsValue, 1000);
-  const debouncedCss = useDebounce(cssValue, 1000);
-
-  useEffect(() => {
-    const output = `<html>
-                    <style>
-                    ${debouncedCss}
-                    </style>
-                    <body>
-                    ${debouncedHtml}
-                    <script type="text/javascript">
-                    ${debouncedJs}
-                    </script>
-                    </body>
-                  </html>`;
-    setOutputValue(output);
-  }, [debouncedHtml, debouncedCss, debouncedJs]);
-
   return (
-    <>
-      <div className={styles.header}>
-        {/*<button className={styles.button} onClick={() => (location.href = "/")}>*/}
-        {/*  New*/}
-        {/*</button>*/}
-        {/*<button className={styles.button} onClick={save}>*/}
-        {/*  {saving ? "Saving..." : "Save"}*/}
-        {/*</button>*/}
-      </div>
-      <SplitPane
-        style={{ marginTop: "60px" }}
-        split="horizontal"
-        minSize={"50%"}
-        onDragFinished={(height) => {
-          setHeightValue(`${height - 40}px`);
-        }}
-      >
-        <SplitPane split="vertical" minSize={"33%"}>
-          <HtmlEditor
-            height={heightValue}
-            value={htmlValue}
-            onChange={setHtmlValue}
-          />
-          <SplitPane split="vertical" minSize={"50%"}>
-            <CssEditor
-              height={heightValue}
-              value={cssValue}
-              onChange={setCssValue}
-            />
-            <JavascriptEditor
-              height={heightValue}
-              value={jsValue}
-              onChange={setJsValue}
-            />
-          </SplitPane>
+    <SplitPane className="h-64" split="horizontal" minSize={"50%"}>
+      <SplitPane split="vertical" minSize={"33%"}>
+        <div>HTML Editor</div>
+        <SplitPane split="vertical" minSize={"50%"}>
+          <div>CSS Editor</div>
+          <div>JS Editor</div>
         </SplitPane>
-        <iframe srcDoc={outputValue} className={styles.previewIframe} />
       </SplitPane>
-    </>
+      <div>Preview Page</div>
+    </SplitPane>
   );
 };
 

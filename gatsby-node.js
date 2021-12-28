a8ac866498a9ee9191d649a5ad139ed34a25ca09
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-27 23:37:02 -0500

    updating test name

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       module: {
         rules: [
           {
-            test: /react-particle-animation/,
+            test: /react-lifecycles-compat/,
             use: loaders.null(),
           },
         ],

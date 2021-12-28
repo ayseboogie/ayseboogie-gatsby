4b69c043b14b514539369041ac365a11f89c8b75
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-28 00:03:56 -0500

    2

--- src/components/codeEditor/codeEditor.js
+++ src/components/codeEditor/codeEditor.js
@@ -3,7 +3,12 @@ import SplitPane from "react-split-pane";
 import * as styles from "./codeEditor.module.css";
 
 const CodeEditor = () => {
-  return <>codeEditor</>;
+  return (
+    <SplitPane split="vertical" minSize={"50%"}>
+      <div>Part 1</div>
+      <div>Part 2</div>
+    </SplitPane>
+  );
 };
 
 export default CodeEditor;

1b399c480d18a1c4908f988f361246524a5ae3aa
Ayse Stinnett <aysestinnett@Ayses-MBP.home>
2021-12-28 20:36:51 -0500

    1.4

--- src/components/codeEditor/codeEditor.js
+++ src/components/codeEditor/codeEditor.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
 import SplitPane from "react-split-pane";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/theme-monokai";
 
 import * as styles from "./codeEditor.module.css";
 
@@ -10,10 +15,22 @@ const CodeEditor = () => {
         <SplitPane split="vertical" minSize={"33%"}>
           <div className={styles.editorContainer}>
             <div className={styles.editorTitle}>HTML</div>
+            <AceEditor
+              mode="html"
+              theme="monokai"
+              name="HTML"
+              setOptions={{ useWorker: false }}
+            />
           </div>
           <SplitPane split="vertical" minSize={"50%"}>
             <div className={styles.editorContainer}>
               <div className={styles.editorTitle}>CSS</div>
+              <AceEditor
+                mode="css"
+                theme="monokai"
+                name="CSS"
+                setOptions={{ useWorker: false }}
+              />
             </div>
             <div>JS Editor</div>
           </SplitPane>
